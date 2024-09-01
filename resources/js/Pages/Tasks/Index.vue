a26de3a50386c17d658403ce5a45b55413a68783
Alfred Nutile <alfrednutile@gmail.com>
2024-08-31 23:31:44 -0400

    remove refresh

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -55,6 +55,14 @@ watch(() => props.chatCompleted, (newValue) => {
 
     <div v-auto-animate>
 
+        <div class="flex justify-end gap-2 items-center" v-if="false">
+            <button type="button"
+                    class="btn btn-outline rounded-none btn-sm"
+                    @click="getTasks" :disabled="getting_tasks">
+                    <span v-if="!getting_tasks">Refresh</span>
+                    <span v-else class="loading loading-dots loading-sm"></span>
+            </button>
+        </div>
 
         <template v-for="task in tasks" :key="task.id">
             <div class="p-2 border border-gray-300 rounded-md my-2">
